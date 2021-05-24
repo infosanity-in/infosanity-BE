e6337791fe0e36a02639dfd6b1b071122043268a
Mayank Khanna <khanna98@outlook.com>
2021-05-25 00:42:20 +0530

    Tags CRUD Ops - MK

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express();
 const contentRouter = require('./content.routes');
+const tagRouter = require('./tags.routes');
 
 router.use('/api/content', contentRouter);
+router.use('/api/tags', tagRouter);
 module.exports = router;
